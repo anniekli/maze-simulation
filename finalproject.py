e4d0e6dd34f02bb67ee69da81a670ecaac559d3b
sugaronsnow <anniekli01@gmail.com>
2017-08-01 13:23:26 -0500

    First change

---
+++ finalproject.py
@@ -0,0 +1,15 @@
+#This is our final project: S.A.M.S.A.L.
+
+from turtle import *
+import math
+
+# Name your Turtle.
+robot = Turtle()
+
+# Set Up your screen and starting position.
+setup(500,300)
+x_pos = -250
+y_pos = -150
+
+
+### Write your code below:
