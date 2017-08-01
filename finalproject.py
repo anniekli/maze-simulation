152f2469a09af9f7783b629d2d038b49770ddf1b
sugaronsnow <anniekli01@gmail.com>
2017-08-01 14:17:27 -0500

    anniee

--- finalproject.py
+++ finalproject.py
@@ -8,10 +8,10 @@ robot = Turtle()
 
 # Set Up your screen and starting position.
 setup(1000,600)
-x_pos = -500
-y_pos = -300
+x_pos = 0
+y_pos = -250
 
-start_pos = (0, -250)
+start_pos = (x_pos, y_pos)
 
 ### Write your code below:
 
